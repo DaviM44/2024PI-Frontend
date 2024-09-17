21a1648f4781704f7a805618c57f7939cfa264d0
DaviM44 <davimatheus.mendonca@outlook.com>
2024-09-16 23:12:01 -0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,25 +9,27 @@ import { TelaLoginComponent } from './tela-login/tela-login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TelaInicialProfComponent } from './tela-inicial-prof/tela-inicial-prof.component';
+import { TelaInicialProfComponent } from './prof/tela-inicial-prof/tela-inicial-prof.component';
 import { AuthpService } from './authp.service';
 import { TelaInicialAdmComponent } from './tela-inicial-adm/tela-inicial-adm.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { TelaLoginAdmComponent } from './tela-login-adm/tela-login-adm.component';
-import { CadastrarProfComponent } from './admin/cadastrar-prof/cadastrar-prof.component';
-import { CadastrarSalaComponent } from './admin/cadastrar-sala/cadastrar-sala.component';
+import { CadastrarProfComponent } from './admin/professor/cadastrar-prof/cadastrar-prof.component';
+import { CadastrarSalaComponent } from './admin/salas/cadastrar-sala/cadastrar-sala.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CadastrarDisciplinasComponent } from './admin/cadastrar-disciplinas/cadastrar-disciplinas.component';
-import { CadastrarCursoComponent } from './admin/cadastrar-curso/cadastrar-curso.component';
-import { GerenciarProfessorComponent } from './admin/gerenciar-professor/gerenciar-professor.component';
-import { GerenciarDisciplinaComponent } from './admin/gerenciar-disciplina/gerenciar-disciplina.component';
-import { GerenciarSalasComponent } from './admin/gerenciar-salas/gerenciar-salas.component';
-import { GerenciarCursosComponent } from './admin/gerenciar-cursos/gerenciar-cursos.component';
-import { EditarDisciplinasComponent } from './admin/editar-disciplinas/editar-disciplinas.component';
-import { EditarProfessorComponent } from './admin/editar-professor/editar-professor.component';
-import { EditarCursoComponent } from './admin/editar-curso/editar-curso.component';
-import { EditarSalasComponent } from './admin/editar-salas/editar-salas.component';
+import { CadastrarDisciplinasComponent } from './admin/disciplinas/cadastrar-disciplinas/cadastrar-disciplinas.component';
+import { GerenciarProfessorComponent } from './admin/professor/gerenciar-professor/gerenciar-professor.component';
+import { GerenciarDisciplinaComponent } from './admin/disciplinas/gerenciar-disciplina/gerenciar-disciplina.component';
+import { GerenciarSalasComponent } from './admin/salas/gerenciar-salas/gerenciar-salas.component';
+import { GerenciarCursosComponent } from './admin/cursos/gerenciar-cursos/gerenciar-cursos.component';
+import { EditarDisciplinasComponent } from './admin/disciplinas/editar-disciplinas/editar-disciplinas.component';
+import { EditarProfessorComponent } from './admin/professor/editar-professor/editar-professor.component';
+import { EditarSalasComponent } from './admin/salas/editar-salas/editar-salas.component';
 import { ReservasComponent } from './admin/reservas/reservas.component';
+import { CadastrarCursoComponent } from './admin/cursos/cadastrar-curso/cadastrar-curso.component';
+import { EditarCursoComponent } from './admin/cursos/editar-curso/editar-curso.component';
+import { PerfilComponent } from './prof/perfil/perfil.component';
+import { NavbarProfComponent } from './navbar-prof/navbar-prof.component';
 
 @NgModule({
   declarations: [
@@ -49,7 +51,9 @@ import { ReservasComponent } from './admin/reservas/reservas.component';
     EditarProfessorComponent,
     EditarCursoComponent,
     EditarSalasComponent,
-    ReservasComponent
+    ReservasComponent,
+    PerfilComponent,
+    NavbarProfComponent
   ],
   imports: [
     BrowserModule,
